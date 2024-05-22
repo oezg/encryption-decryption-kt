4866c2979e42811b3e42283cec4ea6654a355f9d
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-05-22 10:01:51 +0200

    first version

--- Main.kt
+++ Main.kt
@@ -1,5 +1,19 @@
 package encryptdecrypt
 
 fun main() {
-    println("Hello World!")
+    val text = "we found a treasure!"
+    val result = mutableListOf<Char>()
+    for (letter in text) {
+        if (letter.isLetter()) {
+            result.add(cipher(letter))
+        } else {
+            result.add(letter)
+        }
+    }
+    println(result.joinToString(""))
+}
+
+fun cipher(c: Char) : Char {
+    val idx = ('a'..'z').indexOf(c)
+    return ('z' - idx)
 }
