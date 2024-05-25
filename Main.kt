8d0586bd435303de33faf03a8ee266b1e1ffdc53
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-05-25 09:00:51 +0200

    stage 4

--- Main.kt
+++ Main.kt
@@ -1,19 +1,39 @@
 package encryptdecrypt
 
-fun main() {
-    val text = "we found a treasure!"
-    val result = mutableListOf<Char>()
-    for (letter in text) {
-        if (letter.isLetter()) {
-            result.add(cipher(letter))
-        } else {
-            result.add(letter)
-        }
-    }
-    println(result.joinToString(""))
+fun main(args: Array<String>) {
+    val (mode, key, data) = parseArgs(args)
+    require(mode == "enc" || mode == "dec") { "$mode must be 'enc' or 'dec'" }
+    require(key >= 0) { "$key must be positive" }
+
+    if (mode == "enc")
+        data.encrypt(key)
+    else
+        data.decrypt(key)
 }
 
-fun cipher(c: Char) : Char {
-    val idx = ('a'..'z').indexOf(c)
-    return ('z' - idx)
+fun parseArgs(args: Array<String>): Triple<String, Int, String> {
+    val modeIndex = args.indexOf("-mode")
+    var mode = "enc"
+    if (modeIndex >= 0 && modeIndex < args.lastIndex) {
+        mode = args[modeIndex + 1]
+    }
+    val keyIndex = args.indexOf("-key")
+    var key = 0
+    if (keyIndex >= 0 && keyIndex < args.lastIndex) {
+        key = args[keyIndex + 1].toInt()
+    }
+    val dataIndex = args.indexOf("-data")
+    var data = ""
+    if (dataIndex >= 0 && dataIndex < args.lastIndex) {
+        data = args[dataIndex + 1]
+    }
+    return Triple(mode, key, data)
+}
+
+fun String.encrypt(key: Int) {
+    this.map { it + key }.forEach(::print)
+}
+
+fun String.decrypt(key: Int) {
+    this.encrypt(-key)
 }
