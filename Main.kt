5b4e489903d088580df08af8c6635861478c49e2
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-05-28 23:53:21 +0200

    when

--- Main.kt
+++ Main.kt
@@ -1,39 +1,71 @@
 package encryptdecrypt
 
-fun main(args: Array<String>) {
-    val (mode, key, data) = parseArgs(args)
-    require(mode == "enc" || mode == "dec") { "$mode must be 'enc' or 'dec'" }
-    require(key >= 0) { "$key must be positive" }
+import java.io.File
 
-    if (mode == "enc")
-        data.encrypt(key)
-    else
-        data.decrypt(key)
+fun main(args: Array<String>) {
+    val arguments = parseArgs(args)
+    require(arguments.key >= 0) { "Error: ${arguments.key} must be positive" }
+
+    val result = arguments.mode(arguments.data, arguments.key)
+    if (arguments.out == null) {
+        println(result)
+    } else {
+        arguments.out.writeText(result)
+    }
 }
 
-fun parseArgs(args: Array<String>): Triple<String, Int, String> {
+fun parseArgs(args: Array<String>): Arguments {
     val modeIndex = args.indexOf("-mode")
-    var mode = "enc"
-    if (modeIndex >= 0 && modeIndex < args.lastIndex) {
-        mode = args[modeIndex + 1]
+    val mode = when {
+        modeIndex < 0 ||
+        modeIndex == args.lastIndex ||
+        args[modeIndex + 1].startsWith("-") ||
+        args[modeIndex + 1] == "enc" -> ::encrypt
+        args[modeIndex + 1] == "dec" -> ::decrypt
+        else -> throw IllegalArgumentException("Error: mode must be 'enc' or 'dec'")
     }
+
     val keyIndex = args.indexOf("-key")
-    var key = 0
-    if (keyIndex >= 0 && keyIndex < args.lastIndex) {
-        key = args[keyIndex + 1].toInt()
+    val key = when {
+        keyIndex < 0 ||
+        keyIndex == args.lastIndex ||
+        args[keyIndex + 1].startsWith("-") -> 0
+        else -> try {
+            args[keyIndex + 1].toInt()
+        } catch (e: NumberFormatException) {
+            -1
+        }
     }
+    require( key >= 0) { "Error: key must be a positive number" }
+
     val dataIndex = args.indexOf("-data")
     var data = ""
     if (dataIndex >= 0 && dataIndex < args.lastIndex) {
         data = args[dataIndex + 1]
+    } else {
+        val inputIndex = args.indexOf("-in")
+        if (inputIndex >= 0 && inputIndex < args.lastIndex) {
+            val filename = args[inputIndex + 1]
+            data = File(filename).readText()
+        }
     }
-    return Triple(mode, key, data)
+
+    val outIndex = args.indexOf("-out")
+    var out: File? = null
+    if (outIndex >= 0 && outIndex < args.lastIndex) {
+        val filename = args[outIndex + 1]
+        out = File(filename)
+    }
+
+    return Arguments(mode, key, data, out)
 }
 
-fun String.encrypt(key: Int) {
-    this.map { it + key }.forEach(::print)
+fun encrypt(data: String, key: Int): String {
+    return String(data.map { it + key }.toCharArray())
 }
 
-fun String.decrypt(key: Int) {
-    this.encrypt(-key)
+fun decrypt(data: String, key: Int): String {
+    return encrypt(data, -key)
 }
+
+data class Arguments(val mode: (String, Int) -> String, val key: Int, val data: String, val out: File?)
