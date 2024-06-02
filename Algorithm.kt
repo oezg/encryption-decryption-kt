4a227ddece7417ad353da33f8a86e7059e17f3c0
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-06-02 18:05:56 +0200

    inline ranges 'a'..'z'

--- Algorithm.kt
+++ Algorithm.kt
@@ -1,23 +1,18 @@
 package encryptdecrypt
 
-import encryptdecrypt.Shift.width
-
 interface Algorithm {
     fun encrypt(data: String, key: Int): String
     fun decrypt(data: String, key: Int) = encrypt(data, -key)
 }
 
-
 object Shift : Algorithm {
     private const val width = 'z' - 'a' + 1
-    private val lowerCase = 'a'..'z'
-    private val upperCase = 'A'..'Z'
 
     override fun encrypt(data: String, key: Int): String {
         val shift = ((key % width) + width) % width
         return String(
             CharArray(data.length) { idx ->
-                data[idx].shiftForRange(lowerCase, shift).shiftForRange(upperCase, shift)
+                data[idx].shiftForRange('a'..'z', shift).shiftForRange('A'..'Z', shift)
             }
         )
     }
